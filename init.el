21a952bd6d41a4a37cb838043873c94d29b2410c
whythat <yuri.zhykin@gmail.com>
2015-02-26 03:57:08 +0200

    refreshment

--- init.el
+++ init.el
@@ -35,24 +35,9 @@
 ;; autoindentation
 (global-set-key (kbd "RET") 'newline-and-indent)
 
-;; default fullscreen
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized))))
- '(package-selected-packages
-   (quote
-    (elpy smartparens rust-mode haskell-mode paredit slime)))
- '(pyvenv-activate t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; custom interface
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 ;;;-----------------------------------------------------------------------------
 
 
