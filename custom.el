89516c328531c31ff8337a2d18936887cef81c87
whythat <yuri.zhykin@gmail.com>
2015-02-26 03:59:33 +0200

    refreshment

---
+++ custom.el
@@ -0,0 +1,21 @@
+;;;;----------------------------------------------------------------------------
+;;;; custom interface configuration file
+;;;;----------------------------------------------------------------------------
+;; default fullscreen
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (ruby-end elpy smartparens rust-mode haskell-mode paredit slime)))
+ '(pyvenv-activate t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
