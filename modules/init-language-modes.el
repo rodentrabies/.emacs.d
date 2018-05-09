b482973db9adb1a2c070a39439578f353979945d
whythat <whythat@protonmail.com>
2018-05-09 11:42:36 +0300

    Add TeX editing setup

--- modules/init-language-modes.el
+++ modules/init-language-modes.el
@@ -6,6 +6,22 @@
 
 
 ;;;-----------------------------------------------------------------------------
+;;; tex
+;;;-----------------------------------------------------------------------------
+(el-get-bundle auctex
+  (setq TeX-auto-save t)
+  (setq TeX-parse-self t)
+  (setq-default TeX-master nil)
+  (add-hook 'LaTeX-mode-hook 'visual-line-mode)
+  (add-hook 'LaTeX-mode-hook 'flyspell-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t))
+;;;-----------------------------------------------------------------------------
+
+
+
+;;;-----------------------------------------------------------------------------
 ;;; python
 ;;;-----------------------------------------------------------------------------
 (el-get-bundle elpy
